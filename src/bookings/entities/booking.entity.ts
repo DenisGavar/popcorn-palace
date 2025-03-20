1b0a4799719f778693a9c5fca9a1bd740a0ad8bd
DenisGavar <tskhelp@gmail.com>
2025-03-20 20:47:22 +0200

    refactored swagger documentation

--- src/bookings/entities/booking.entity.ts
+++ src/bookings/entities/booking.entity.ts
@@ -3,35 +3,19 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class BookingEntity implements Booking {
   @ApiProperty({
-    example: 1,
+    example: 'd1a6423b-4469-4b00-8c5f-e3cfc42eacae',
     description: 'The unique identifier of the booking',
+    name: 'bookingId',
   })
   id: string;
 
-  @ApiProperty({
-    example: 1,
-    description: 'The ID of the showtime associated with the booking',
-  })
   showtimeId: number;
 
-  @ApiProperty({ example: 15, description: 'The seat number for the booking' })
   seatNumber: number;
 
-  @ApiProperty({
-    example: '84438967-f68f-4fa0-b620-0f08217e76af',
-    description: 'The unique identifier of the user making the booking',
-  })
   userId: string;
 
-  @ApiProperty({
-    example: '2025-02-14T14:47:46.125Z',
-    description: 'The date and time when the booking record was created',
-  })
   createdAt: Date;
 
-  @ApiProperty({
-    example: '2025-02-15T10:20:30.456Z',
-    description: 'The date and time when the booking record was last updated',
-  })
   updatedAt: Date;
 }
